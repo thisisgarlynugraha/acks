e61248576c84443cb285d65c10fe7e94df0afd5a
thisisgarlynugraha <thisisgarlynugraha@gmail.com>
2024-11-15 15:20:37 +0700

    FIX : View - Profile

--- resources/views/layouts/components/master/navbar.blade.php
+++ resources/views/layouts/components/master/navbar.blade.php
@@ -12,6 +12,7 @@
     <ul class="navbar-nav navbar-right">
         <li class="dropdown">
             <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <img alt="image" src="{{ asset('vendor/stisla-2.2.0/dist/assets/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
                 <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div>
             </a>
 
