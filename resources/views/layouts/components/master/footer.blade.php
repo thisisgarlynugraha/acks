6c02966aca7c4a9beea20a5784bb3824ce986f0b
thisisgarlynugraha <thisisgarlynugraha@gmail.com>
2024-11-15 10:29:33 +0700

    FIX : View - Dashboard

---
+++ resources/views/layouts/components/master/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <div class="footer-left">Copyright &copy; ACKS - 2024</div>
+    <div class="footer-right">Template by <a href="https://nauval.in/">Muhamad Nauval Azhar</a> & Develop by <a href="https://arcakranusantara.com/">Arcakranusantara</a></div>
+</footer>
---
+++ resources/views/layouts/components/master/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <img class="d-inline-block" width="32px" height="30.61px" src="{{ asset('images/logo/acks.png') }}" alt="">
+        </div>
+        
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="{{ route('dashboard') }}">
+                <img class="d-inline-block" width="32px" height="30.61px" src="{{ asset('images/logo/acks.png') }}" alt="">
+            </a>
+        </div>
+
+        <ul class="sidebar-menu">
+            <li class="menu-header">{{ __('Master') }}</li>
+            <li class="{{ Request::routeIs('dashboard') ? 'active' : '' }}"><a class="nav-link" href="{{ route('dashboard') }}"><i class="fas fa-fire"></i> <span>{{ __('Dashboard') }}</span></a></li>
+        </ul>
+    </aside>
+</div>
