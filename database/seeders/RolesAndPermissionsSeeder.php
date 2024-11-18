01dbbf38f685f5b63304726fa74b9fec9ee2c60c
thisisgarlynugraha <thisisgarlynugraha@gmail.com>
2024-11-18 11:41:13 +0700

    FIX : RolesAndPermissionsSeeder

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\PermissionRegistrar;
@@ -12,7 +13,41 @@ class RolesAndPermissionsSeeder extends Seeder
     {
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
+        $permissions = [
+            'Dashboard',
+            'Student - Read',
+            'Student - Create',
+            'Student - Update',
+            'Student - Delete',
+            'Student Photo - Read',
+            'Student Photo - Create',
+            'Student Photo - Delete',
+            'Attendance - Read',
+            'Attendance - Detail',
+            'Health Monitoring - Read',
+            'Health Monitoring - Detail',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
         $Administrator = Role::create(['name' => 'administrator', 'guard_name' => 'web']);
+        $Administrator->givePermissionTo('Student - Read');
+        $Administrator->givePermissionTo('Student - Create');
+        $Administrator->givePermissionTo('Student - Update');
+        $Administrator->givePermissionTo('Student - Delete');
+        $Administrator->givePermissionTo('Student Photo - Read');
+        $Administrator->givePermissionTo('Student Photo - Create');
+        $Administrator->givePermissionTo('Student Photo - Delete');
+        $Administrator->givePermissionTo('Attendance - Read');
+        $Administrator->givePermissionTo('Attendance - Detail');
+        $Administrator->givePermissionTo('Health Monitoring - Read');
+        $Administrator->givePermissionTo('Health Monitoring - Detail');
+
         $Student = Role::create(['name' => 'student', 'guard_name' => 'web']);
+        $Student->givePermissionTo('Attendance - Detail');
+        $Student->givePermissionTo('Health Monitoring - Detail');
+
     }
 }
