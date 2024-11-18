788cf04e772ec85b1325cdc3b4c833fd96b0c344
thisisgarlynugraha <thisisgarlynugraha@gmail.com>
2024-11-18 15:29:48 +0700

    FIX : UsersSeeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -15,7 +15,7 @@ class UsersSeeder extends Seeder
             'name' => 'Administrator ACKS',
             'email' => 'administrator@acks.my.id',
             'email_verified_at' => now(),
-            'password' => Hash::make('Adm!nBuk@n5*mb4ran9Adm?n'),
+            'password' => Hash::make('Adm!nAdm?n'),
             'remember_token' => Str::random(10),
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
